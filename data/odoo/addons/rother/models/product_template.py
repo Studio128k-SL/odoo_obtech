f294ec09c21748a002a19cf6aa72764c6c9c8519
Alexis <alexis.barrera@rotherindustrial.es>
2026-06-05 14:32:59 +0200

    Creado botón para realizar traslados de inventario dentro de un Producto

--- data/odoo/addons/rother/models/product_template.py
+++ data/odoo/addons/rother/models/product_template.py
@@ -1,6 +1,7 @@
 from odoo import models, fields, api
 import base64
 
+
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
@@ -34,6 +35,63 @@ class ProductTemplate(models.Model):
                 record.x_display_name_label = record.name[:60] + '...'
             else:
                 record.x_display_name_label = record.name or ''
+
+    def action_realizar_traslado(self):
+        self.ensure_one()
+
+        quants = self.env['stock.quant'].search([
+            ('product_id.product_tmpl_id', '=', self.id),
+            ('quantity', '>', 0),
+            ('location_id.usage', '=', 'internal'),
+        ])
+        location_ids = quants.mapped('location_id').ids
+
+        picking_type = self.env['stock.picking.type'].search([
+            ('code', '=', 'internal'),
+            ('company_id', '=', self.env.company.id),
+        ], limit=1)
+
+        picking = self.env['stock.picking'].create({
+            'picking_type_id': picking_type.id,
+            'location_id': location_ids[0] if location_ids else picking_type.default_location_src_id.id,
+            'location_dest_id': picking_type.default_location_dest_id.id,
+            'move_ids': [(0, 0, {
+                'name': self.name,
+                'product_id': self.product_variant_ids[0].id,
+                'product_uom_qty': 1,
+                'product_uom': self.uom_id.id,
+                'picking_type_id': picking_type.id,
+                'location_id': location_ids[0] if location_ids else picking_type.default_location_src_id.id,
+                'location_dest_id': picking_type.default_location_dest_id.id,
+            })],
+        })
+
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Realizar Traslado',
+            'res_model': 'stock.picking',
+            'res_id': picking.id,
+            'view_mode': 'form',
+            'view_id': self.env.ref('rother.rother_stock_picking_traslado_form').id,
+            'context': dict(self.env.context, allowed_location_ids=location_ids),
+        }
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    allowed_location_ids = fields.Many2many(
+        'stock.location',
+        string='Allowed Source Locations',
+        compute='_compute_allowed_location_ids',
+        store=False,
+    )
+
+    @api.depends_context('allowed_location_ids')
+    def _compute_allowed_location_ids(self):
+        for picking in self:
+            ids_ = picking.env.context.get('allowed_location_ids', [])
+            picking.allowed_location_ids = [(6, 0, ids_)]
 
 
 class ProductProduct(models.Model):
